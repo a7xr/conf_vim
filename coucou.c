f78e241a77a2ecbbab7844717a1a8cb29c38fd30
nonicken <solofomamitiana0@gmail.com>
2015-03-09 07:58:53 +0200

    going to set vim as a c_ide

---
+++ coucou.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  coucou.c
+ *
+ *    Description:  this is a test
+ *
+ *        Version:  1.0
+ *        Created:  09/03/2015 07:51:01
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  mamtina_solofo (mamtina), mehner@fh-swf.de
+ *        Company:  no_company_yet
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main(int argc, char *argv[]){ 
+
+	printf("this is a test");
+	return 0;
+}
+
+
